e02c42fd623328545a880bc1b8c037a2d8590b0c
Omar <omarmeciry21@gmail.com>
2022-03-27 22:44:03 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+//        try {
+//            Scanner sc = new Scanner(System.in);
+//            int  numerator = sc.nextInt();
+//            int dominator = sc.nextInt();
+//            System.out.println(numerator/dominator);
+//            System.out.println("----Inner Line----");
+//        }
+//        catch (ArithmeticException e){
+//            System.err.println("Can't Divide by zero!!!");
+//        }
+//        catch (InputMismatchException e){
+//
+//            System.err.println("Invalid Input!!!");
+//        }
+//        catch (Exception e){
+//            System.err.println("An error occurred!!!");
+//        }
+//
+//        System.out.println("----Final Line----");
+
+//        try{
+//            printDataWithPrinter();
+//        }
+//        catch (UnavailablePrinterException e){
+//            System.err.println(e.message);
+//        }
+//    }
+//
+//    static void printDataWithPrinter() throws UnavailablePrinterException{
+//        UnavailablePrinterException e = new UnavailablePrinterException("This printer is currently unavialable!!!",400);
+//        throw e;
+//    }
+}
+
+//class UnavailablePrinterException extends Exception{
+//    String message;
+//    int code;
+//
+//    UnavailablePrinterException(String m, int c){
+//        message=m;
+//        code=c;
+//    }
+//}
+
+
+//class A{}
+//class B extends  A{}
+//class C extends  A{}
